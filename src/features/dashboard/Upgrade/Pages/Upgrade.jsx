c810d139580acd8786d0625f32501b794d5f850d
krishmamaharjan <krishmamaharjan73@gmail.com>
2025-10-06 12:24:39 +0545

    fix: grid col

--- src/features/dashboard/Upgrade/Pages/Upgrade.jsx
+++ src/features/dashboard/Upgrade/Pages/Upgrade.jsx
@@ -81,10 +81,10 @@ export default function Upgrade()
                 <div className="pt-10">
                     <h1 className="font-semibold text-2xl font-">All paid plans include:</h1>
 
-                    <div className="grid grid-cols-1 lg:grid-cols-2 pt-5 pr-10 gap-10 ">
+                    <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 pt-5  gap-10 ">
                         <div>
                             <h1 className="text-xl ">Onboarding</h1>
-                            <ul className="list-none text-textgray">
+                            <ul className="list-none text-gray-500">
                                 <li>Structured onboarding information by e-mail(from Growth)</li>
                                 <li>Phone onboarding support(from Complete)</li>
                                 <li>Event creation process for fast event creation</li>
@@ -93,7 +93,7 @@ export default function Upgrade()
 
                         <div>
                             <h1 className="text-xl ">Customer Success Team</h1>
-                            <ul className="list-none text-textgray">
+                            <ul className="list-none text-gray-500">
                                 <li>Access to our premium support service</li>
                                 <li>Immediate call-back support for issues</li>
                                 <li>Dedicated Customer Success Manager(from Complete)</li>
@@ -102,7 +102,7 @@ export default function Upgrade()
 
                         <div>
                             <h1 className="text-xl ">Data Protection & Compliance</h1>
-                            <ul className="list-none text-textgray">
+                            <ul className="list-none text-gray-500">
                                 <li>Data encryption via SSL/TLS L2 with SHA-256</li>
                             </ul>
                         </div>
